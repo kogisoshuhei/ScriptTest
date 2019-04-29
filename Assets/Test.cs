45045f839a418825c98f53af1d4744f2bfbf0a6d
shuhei kogiso <doodles9150@gmail.com>
2019-04-29 23:38:54 +0900

    ScriptTest 2回目
    Lesson4の課題（GameObject）及び発展課題の修正分（GameObject2）になります。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,50 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Magic {        
-	private int mp = 53; // 魔力
-
-	// 魔法攻撃用の関数
-	public void Attack(int decrease) {
-
-		//残り魔力
-		Debug.Log ("現在のMPは" + mp + "。");
-
-		//魔力の消費をカウント
-		for (int i = mp ; i > decrease ; i -= decrease ) {
-
-			// 残りmpを減らす
-			this.mp -= decrease;
-
-			//魔法攻撃結果
-			Debug.Log ("魔法攻撃をした。残りMPは" + mp + "。");
-
-		}
-		//mpが足りない
-		Debug.Log ("MPが足りないため魔法が使えない。");
-	}
-
-}
-
 public class Test : MonoBehaviour {
 
 	void Start(){
 
 		//課題１
-		/*
-		int[] array = { 1, 2, 3, 4, 5, 6 };
+		int[] array = { 30, 20, 60, 80, 10 };
 
-		for (int i = 5; i >= 0; i--) {
+		Debug.Log ( "昇順" );
+
+		for (int i = 0; i <= 4; i++) {
 
 			Debug.Log ( array [i] );
-		*/
 
-		// Magicクラスの変数を宣言してインスタンスを代入
-		Magic decreasePt = new Magic ();
+		}
 
-		// 消費用の関数を呼び出し、消費MPを指定
-		decreasePt.Attack( 5 );
+		Debug.Log ( "逆順" );
+
+		for (int i = 4; i >= 0; i--) {
+		
+			Debug.Log ( array [i] );
+		
+		}
 
 	}
-				
+
 }
---
+++ Assets/Test2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic {
+	
+	private int mp = 53; // MP
+
+	//
+	public void Decrease(int decrease) {
+
+		for (int i = 1; i <= 11; i++){
+
+			if (mp >= decrease) {
+
+				this.mp -= decrease;
+
+				Debug.Log ("魔法攻撃をした。残りMPは" + this.mp + "。");
+
+			} else {
+			
+				Debug.Log ("MPが足りないため魔法が使えない。");
+
+			}
+
+		}
+		
+	}
+
+}
+
+public class Test2 : MonoBehaviour {
+
+	void Start () {
+		
+		// Magicクラスの変数を宣言してインスタンスを代入
+		Magic decreasePt = new Magic ();
+
+		// 魔法消費用の関数を呼び出す
+		decreasePt.Decrease( 5 );
+	
+	}
+
+}
