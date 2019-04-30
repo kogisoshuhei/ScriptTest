d935f40c9e05eb00ea9022b4ebc5052e8707d8de
shuhei kogiso <doodles9150@gmail.com>
2019-04-30 17:17:52 +0900

    ScriptTest 3回目
    Lesson4の発展課題の修正分(Magic関数内での処理)になります。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 
-	void Start(){
+	void Start () {
 
-		//課題１
+		//通常課題
 		int[] array = { 30, 20, 60, 80, 10 };
 
 		Debug.Log ( "昇順" );
@@ -20,9 +20,44 @@ public class Test : MonoBehaviour {
 		Debug.Log ( "逆順" );
 
 		for (int i = 4; i >= 0; i--) {
-		
+
 			Debug.Log ( array [i] );
-		
+
+		}
+
+		//発展課題
+		// Bossクラスの変数を宣言してインスタンスを代入
+		Boss decreasePt = new Boss ();
+
+		// 魔法消費用の関数を呼び出す
+		decreasePt.Magic( 5 );
+
+	
+	}
+
+	public class Boss {
+
+		private int mp = 53; // MP
+
+		//
+		public void Magic(int decrease) {
+
+			for (int i = 0; i <= 10; i++){
+
+				if (mp >= decrease) {
+
+					this.mp -= decrease;
+
+					Debug.Log ("魔法攻撃をした。残りMPは" + this.mp + "。");
+
+				} else {
+
+					Debug.Log ("MPが足りないため魔法が使えない。");
+
+				}
+
+			}
+
 		}
 
 	}
